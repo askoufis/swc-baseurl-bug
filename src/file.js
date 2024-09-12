cdf5b5ea058c1095f3afebf1b1bc67432fb9de0d
Adam Skoufis <askoufis@seek.com.au>
2024-09-12 11:16:46 +1000

    Add import from src

--- src/file.js
+++ src/file.js
@@ -1,4 +1,5 @@
 export { dotFoo } from "./.foo";
 export { foo } from "./foo";
+export { foo as fooReExport } from "src/foo";
 export { notReal } from "./.notReal";
 export { fooFoo } from "./foo.foo";
